e05d7c0f5099d4ccee4dc6ecf402fa52f972bc8c
horensen <5269379+horensen@users.noreply.github.com>
2022-01-23 20:44:09 +0800

    Fulfil intent

--- functions/intents/demo3Covid19Recommendations.js
+++ functions/intents/demo3Covid19Recommendations.js
@@ -1,3 +1,8 @@
+require("dotenv").config();
+
+const Airtable = require("airtable");
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base("appq1WXSu5RRK5iYI");
+
 const contexts = require("../contexts");
 const utils = require("../utils");
 const replies = require("../replies");
@@ -18,28 +23,70 @@ module.exports = (request) => {
   const symptomsDuration = parameters.slot_symptoms_duration || sessionContextParams.symptoms_duration;
 
   return (agent) => {
-    // Reply based on parameter permutation
-    replies.replyCovid19Recommendations(
-      agent,
-      age,
-      treatment,
-      legallyAtRisk,
-      covid19TestResult,
-      symptoms,
-      symptomsDuration
-    );
+    const baseExamplesPromise = new Promise(async (resolve, reject) => {
+      const tableExamples = [];
 
-    // Update conversation context
-    const newContextParams = {
-      currentTopic: "covid19",
-      age,
-      treatment,
-      legallyAtRisk,
-      covid19TestResult,
-      symptoms,
-      symptomsDuration
-    };
-    const lifespan = 5;
-    utils.updateContextParameters(request, agent, contexts.SESSION, newContextParams, lifespan);
+      await base("Examples")
+        .select({ maxRecords: 50, view: "Grid view" })
+        .eachPage(
+          (records, fetchNextPage) => {
+            records.forEach((record) => tableExamples.push(record._rawJson.fields));
+            fetchNextPage();
+          },
+          async (err) => {
+            if (err) reject(err);
+            resolve(tableExamples);
+          }
+        );
+    });
+
+    const baseRecommendationsPromise = new Promise(async (resolve, reject) => {
+      const tableRecommendations = [];
+
+      await base("Recommendations")
+        .select({ maxRecords: 50, view: "Grid view" })
+        .eachPage(
+          (records, fetchNextPage) => {
+            records.forEach((record) => tableRecommendations.push(record._rawJson));
+            fetchNextPage();
+          },
+          (err) => {
+            if (err) reject(err);
+            resolve(tableRecommendations);
+          }
+        );
+    });
+
+    const promises = [baseExamplesPromise, baseRecommendationsPromise];
+
+    return Promise.all(promises).then((values) => {
+      const [examples, recommendations] = values;
+      if (examples && recommendations) {
+        // Reply based on parameter permutation
+        replies.replyCovid19Recommendations(
+          agent,
+          examples,
+          recommendations,
+          age,
+          treatment,
+          legallyAtRisk,
+          covid19TestResult,
+          symptoms,
+          symptomsDuration
+        );
+        // Update conversation context
+        const newContextParams = {
+          currentTopic: "covid19",
+          age,
+          treatment,
+          legallyAtRisk,
+          covid19TestResult,
+          symptoms,
+          symptomsDuration
+        };
+        const lifespan = 5;
+        utils.updateContextParameters(request, agent, contexts.SESSION, newContextParams, lifespan);
+      }
+    });
   };
 };
--- functions/replies/replyCovid19Recommendations.js
+++ functions/replies/replyCovid19Recommendations.js
@@ -1,8 +1,9 @@
-const services = require("../services");
-const utils = require("../utils");
+const DecisionTree = require("decision-tree");
 
 const replyCovid19Recommendations = (
   agent,
+  examples,
+  recommendations,
   age,
   treatment,
   legallyAtRisk,
@@ -10,10 +11,90 @@ const replyCovid19Recommendations = (
   symptoms,
   symptomsDuration
 ) => {
+  let age_group = "17-64"; // default
+  if (age >= 65) age_group = ">=65";
+  if (age >= 17 && age <= 64) age_group = "17-64";
+  if (age >= 13 && age <= 16) age_group = "13-16";
+  if (age <= 12) age_group = "<=12";
 
-  agent.add(
-    `${age}, ${treatment}, ${legallyAtRisk}, ${covid19TestResult}, ${JSON.stringify(symptoms)}, ${symptomsDuration}`
-  );
+  let has_chronic_disease = false; // default
+  let is_under_dialysis_or_chemo = false; // default
+  if (treatment === "dialysis" || treatment === "chemotherapy") {
+    has_chronic_disease = true;
+    is_under_dialysis_or_chemo = true;
+  } else if (treatment === "other treatment") {
+    has_chronic_disease = true;
+  }
+
+  let is_legally_at_risk = false; // default
+  if (legallyAtRisk === "true") is_legally_at_risk = true;
+  if (legallyAtRisk === "false") is_legally_at_risk = false;
+
+  let covid19_test = "no_result_past_48_hours"; // default
+  if (covid19TestResult === "positive") covid19_test = "positive";
+  if (covid19TestResult === "negative") covid19_test = "negative";
+
+  const has_breathing_difficulty = symptoms.includes("difficulty breathing");
+  const has_cough = symptoms.includes("cough");
+  const has_fever = symptoms.includes("fever");
+  const has_taste_smell_loss = symptoms.includes("loss of taste or smell");
+  const has_sore_throat = symptoms.includes("sore throat");
+  const has_diarrhoea = symptoms.includes("diarrhoea");
+  const has_body_ache = symptoms.includes("body ache");
+  const has_runny_nose = symptoms.includes("runny nose");
+
+  let duration = "5-7"; // default
+  if (symptomsDuration.unit === "day") {
+    const days = symptomsDuration.amount;
+    if (days > 14) duration = ">14";
+    if (days >= 8 && days <= 14) duration = "8-14";
+    if (days >= 5 && days <= 7) duration = "5-7";
+    if (days <= 4) duration = "0-4";
+  }
+
+  const data_point = {
+    age_group,
+    has_chronic_disease,
+    is_under_dialysis_or_chemo,
+    is_legally_at_risk,
+    covid19_test,
+    has_breathing_difficulty,
+    has_cough,
+    has_fever,
+    has_taste_smell_loss,
+    has_sore_throat,
+    has_diarrhoea,
+    has_body_ache,
+    has_runny_nose,
+    duration
+  };
+
+
+  const dt = new DecisionTree("recommendation", [
+    "age_group",
+    "has_chronic_disease",
+    "is_under_dialysis_or_chemo",
+    "is_legally_at_risk",
+    "covid19_test",
+    "has_breathing_difficulty",
+    "has_cough",
+    "has_fever",
+    "has_taste_smell_loss",
+    "has_sore_throat",
+    "has_diarrhoea",
+    "has_body_ache",
+    "has_runny_nose",
+    "duration"
+  ]);
+
+  dt.train(examples);
+
+  const prediction = dt.predict(data_point);
+  const recommendation_id = prediction[0];
+
+  const recommendation = recommendations.filter(r => r.id === recommendation_id)[0];
+
+  agent.add(recommendation.fields.description);
 };
 
 module.exports = replyCovid19Recommendations;
